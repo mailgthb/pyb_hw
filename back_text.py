888023cb8bf6d1d7302455674c1a8dff430d465f
iagthb <mailgthb@gmail.com>
2021-06-27 23:36:21 +0300

    Убрал лишние пробелы внутри скобок. Согласно PEP8

--- back_text.py
+++ back_text.py
@@ -7,17 +7,17 @@ LEN_DISPLAY = 85
 
 line = '"...і що сал ?о лас ощ і"'
 print()
-line_to_print = (Fore.GREEN + START_TITLE_PROGRAM + Style.RESET_ALL)
+line_to_print = Fore.GREEN + START_TITLE_PROGRAM + Style.RESET_ALL
 line_to_print += line + Fore.GREEN + '! =======' + Style.RESET_ALL
 print(line_to_print)
 
 # Друк коду першого способу
 print('\n')
-print( Fore.CYAN + "Перший спосіб".center(LEN_DISPLAY, '-') + Fore.RESET )
-print( Fore.BLUE + "line[::-1]", end = Fore.CYAN + '  :     ' )
-print( Fore.RESET + "   ", end = '' )
+print(Fore.CYAN + "Перший спосіб".center(LEN_DISPLAY, '-') + Fore.RESET)
+print(Fore.BLUE + "line[::-1]", end = Fore.CYAN + '  :     ')
+print(Fore.RESET + "   ", end = '')
 # Сам код
-print( line[::-1] ) 
+print(line[::-1]) 
 
 
 # Друк коду другого способу
@@ -25,8 +25,8 @@ print('\n')
 print(Fore.CYAN + "Другий спосіб".center(LEN_DISPLAY, '-'), end = '')
 print(Fore.BLUE + """
 for elem in reversed(line):
-    print(elem, end = '') """, end = Fore.CYAN + ' :     ' )
-print( Fore.RESET, end = '')
+    print(elem, end = '') """, end = Fore.CYAN + ' :     ')
+print(Fore.RESET, end = '')
 # Сам код
 for elem in reversed(line):
     print(elem, end = '')
@@ -50,12 +50,12 @@ print(Fore.CYAN + "Четвертий спосіб".center(LEN_DISPLAY, '-'), en
 codeText = """
 Lline = list(line)
 for i in range(len(line) // 2):
-    Lline[i], Lline[ -1 - i] = Lline[-1 -i], Lline[i]"""
+    Lline[i], Lline[-1 - i] = Lline[-1 -i], Lline[i]"""
 print(Fore.BLUE + codeText, end = '')                  
 print(Fore.CYAN + " : " + Style.RESET_ALL, end = '')
 # Сам код
 Lline = list(line)
 for i in range(len(line) // 2):
-    Lline[i], Lline[ -1 - i] = Lline[-1 -i], Lline[i]
+    Lline[i], Lline[-1 - i] = Lline[-1 -i], Lline[i]
 print(''.join(Lline))
 

1954b0f9d6726de4da7c736ddd264464aab07595
iagthb <mailgthb@gmail.com>
2021-06-28 23:38:01 +0300

    Прибрав лишнє, то що сховав, коли закоментував. Прибрав лишні пусті рядки. Добавив до дозволених символів апостроф - поставив в кінці абетки. При виводі на екран результату - вказав, що виводиться.

--- ordered_substring.py
+++ ordered_substring.py
@@ -1,32 +1,25 @@
 import colorama
 from colorama import Fore, Back, Style
 colorama.init()
-ABETKA = "абвгґдеєжзиіїйклмнопрстуфхцчшщюяь"
+
+ABETKA = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'"
 
 line = input("Введіть текстовий рядок (без пробілів, "
-             "розділових знаків, інших символів, лише букви української мови): ").lower()
+             "розділових знаків, інших символів, "
+             "лише букви української мови та апостроф): ").lower()
 longest_ordered_substring = ''
 num_pos = -1
 longest_ordered_substring_result = ''
 
 for letter in line:
-    
-    
-##    if letter == ' ':
-##        print("Рядок повинен бути без пробілів!")
-##        break
-    
     num_pos_prev = num_pos
-    
-    
     num_pos = ABETKA.index(letter)
     if num_pos >= num_pos_prev:
         longest_ordered_substring += letter
     else:
         if len(longest_ordered_substring) > len(longest_ordered_substring_result):
             longest_ordered_substring_result = longest_ordered_substring
-        
         longest_ordered_substring = letter
-print(longest_ordered_substring_result)  
+print("Підрядок максимальної довжини, в якому літери впорядковані в алфавітному порядку є: ", longest_ordered_substring_result)  
     
     

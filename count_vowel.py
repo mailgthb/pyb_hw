e280a2c3a5fdb458fd4973e6374151e8be5a5065
iagthb <mailgthb@gmail.com>
2021-06-28 17:15:46 +0300

    Добавив, щоб підраховувало також великі голосні літери. Просто введений рядок перевів у маленькі символи

--- count_vowel.py
+++ count_vowel.py
@@ -4,12 +4,10 @@ colorama.init()
 
 VOWELS = "уеіїаоєяию"
 
-line = input("Наберіть текстовий рядок: ")
+line = input("Наберіть текстовий рядок: ").lower()
 num_vowels = 0
 for vowel in VOWELS:
     for symb in line:
         if vowel == symb:
             num_vowels += 1
 print(num_vowels)
-
-

5c899714b3bde453dff4dba6658dd7a4a684334d
iagthb <mailgthb@gmail.com>
2021-09-26 21:34:47 +0300

    Замінив for на while і начало нормально рахувати, однак якщо елемент зустрічається один раз, то його не бере до уваги, крім останього

--- ypr/count_calls/tet_copy.py
+++ ypr/count_calls/tet_copy.py
@@ -3,24 +3,17 @@ lst_tel0 = (442362910, 442362910, 442577227, 443334023, 443334023,\
 	443334023, 443334023, 443334023, 443334023, 443334023, \
 	443544131, 443923277, 443923277, 445191919, 462601376)
 
-dct_num_rep = {}
+dct_fruit = {}
 n=0
 while n<len(lst_tel0):
 	elm = lst_tel0[n]
-	dct_num_rep[elm] = 1
-	print(dct_num_rep)
-	k = n + 1
-	while k<len(lst_tel0):
-		if elm == lst_tel0[k]:
-			dct_num_rep[elm] += 1
+	dct_fruit[elm] = 1
+	print(dct_fruit)
+	t = n+1
+	for k, item_to_check in enumerate(lst_tel0[t:]):
+		if elm == item_to_check:
+			dct_fruit[elm] += 1
 		else:
-			n=k
+			n=k+1
 			break
-		k += 1
 	n += 1
-
-
-	
-	
-	
-	
